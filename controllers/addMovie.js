b784da68940761c946ba8e394f249ee5df97ee1a
Megagig <megagigsoftwaresolutions.com>
2024-10-18 23:57:25 +0100

    Using try catch to handle errors & exceptions

--- controllers/addMovie.js
+++ controllers/addMovie.js
@@ -3,6 +3,17 @@ const addMovie = (req, res) => {
 
   //validation
 
+  try {
+    if (!movie_name || !info || !rating)
+      throw 'Movie, info and rating is required';
+    if (rating < 1 || rating > 10) throw 'rating must be between 1 to 10';
+  } catch (error) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please provide movie_name, info and rating',
+    });
+  }
+
   // success
   res.status(200).json({
     status: 'sucess',
