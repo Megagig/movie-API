e805c50c895ec4409077c8d4f926424910362903
Megagig <megagigsoftwaresolutions.com>
2024-10-20 18:56:50 +0100

    Deleting data and movie

--- controllers/deleteMovie.js
+++ controllers/deleteMovie.js
@@ -1,3 +1,28 @@
-const deleteMovie = async () => {};
+const mongoose = require('mongoose');
+
+const deleteMovie = async (req, res) => {
+  const moviesModel = mongoose.model('movies');
+  const movie_id = req.params.movie_id; // Assuming the movie ID is passed as a URL parameter
+
+  try {
+    const deletedMovie = await moviesModel.findByIdAndDelete(movie_id);
+    if (!deletedMovie) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Movie not found',
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      message: 'Movie deleted successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'An error occurred while deleting the movie',
+    });
+    return;
+  }
+};
 
 module.exports = deleteMovie;
