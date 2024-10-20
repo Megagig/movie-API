a469a4818cca4d8057ee926bcfa1b98fb5c83816
Megagig <megagigsoftwaresolutions.com>
2024-10-20 12:49:12 +0100

    GetALLmovies route

--- controllers/getAllMovies.js
+++ controllers/getAllMovies.js
@@ -1,3 +1,12 @@
-const getAllMoves = () => {};
+const mongoose = require('mongoose');
+const getAllMoves = async (req, res) => {
+  const moviesModel = mongoose.model('movies');
+  const allMovies = await moviesModel.find({});
+  res.status(200).json({
+    status: 'success',
+    data: allMovies,
+    message: 'All movies fetched successfully',
+  });
+};
 
 module.exports = getAllMoves;
